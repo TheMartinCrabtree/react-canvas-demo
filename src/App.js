de79dee2a2fa606771f58ef908ef8acf9e510f09
Martin Crabtree <mcrabtr33@gmail.com>
2020-02-26 16:23:01 -0500

    added canvas and basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
 import './App.css';
 
 function App() {
   return (
-    <main>
-      
+    <main className="App-main" >
+
+      <canvas className="App-canvas"
+        width={window.innerWidth * .5}
+        height={window.innerHeight * .5}
+        onClick={
+          (event)=>{alert("You have clicked" + event.clientX + event.clientY)}
+        } />
+
     </main>
   );
 }
