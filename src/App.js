bd34e5dc8f02ec9cee99fe0ce234e8de55e68b11
MC-Games\MC <martin.crabtree@gmail.com>
2020-02-27 22:58:24 -0500

    moved canvas logic to custom hook

--- src/App.js
+++ src/App.js
@@ -1,54 +1,21 @@
 import React, {useRef, useState, useEffect} from 'react';
+import { useCanvas } from './hooks/useCanvas';
 import './App.css';
 
-
-// Path2D SVG for a heart
-const heartSVG = "M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0 z"
-const SVG_PATH = new Path2D(heartSVG);
-
-
-// Scaling Constants for Canvas
-const SCALE = 0.3;
-const OFFSET = 80;
-const canvasWidth = window.innerWidth * .5;
-const canvasHeight = window.innerHeight * .5;
-
-function draw(ctx, location){
-  console.log("attempting to draw")
-  ctx.fillStyle = 'red';
-  ctx.shadowColor = 'blue';
-  ctx.shadowBlur = 15;
-  ctx.save();
-  ctx.scale(SCALE, SCALE);
-  ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET);
-  ctx.rotate(225 * Math.PI / 180);
-  ctx.fill(SVG_PATH);
-  // .restore(): Canvas 2D API restores the most recently saved canvas state
-  ctx.restore();  
-};
-
-
 function App() {
-  // useRef creates an JS object that can be manipulated and is not recreated each render
-  const canvasRef = useRef(null);
+  
 
-  const [coordinates, setCoordinates] = useState([]);
-
-  useEffect(()=>{
-    const canvasObj = canvasRef.current;
-    const ctx = canvasObj.getContext('2d');
-    ctx.clearRect( 0,0, canvasHeight, canvasWidth );
-
-    // draw all coordinates held in state
-    coordinates.forEach((coordinate)=>{draw(ctx, coordinate)});
-  })
+  const [ coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight ] = useCanvas();
 
   const handleCanvasClick=(event)=>{
     // on each click get current mouse location 
     const currentCoord = { x: event.clientX, y: event.clientY };
     // add the newest mouse location to an array in state 
-    setCoordinates([...coordinates, currentCoord])
-    console.log("Coordinates", coordinates);
+    setCoordinates([...coordinates, currentCoord]);
+  };
+
+  const handleClearCanvas=(event)=>{
+    setCoordinates([]);
   };
 
 
@@ -61,6 +28,10 @@ function App() {
         width={canvasWidth}
         height={canvasHeight}
         onClick={handleCanvasClick} />
+
+      <div className="button" >
+        <button onClick={handleClearCanvas} > CLEAR </button>
+      </div>
 
     </main>
   );
