816ee7daa10afb97c4f508473f4686549113e0f9
MC-Games\MC <martin.crabtree@gmail.com>
2020-02-27 20:45:22 -0500

    saving coordinates to state and drawing in useEffect

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ const heartSVG = "M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0
 const SVG_PATH = new Path2D(heartSVG);
 
 
-
 // Scaling Constants for Canvas
 const SCALE = 0.3;
 const OFFSET = 80;
+const canvasWidth = window.innerWidth * .5;
+const canvasHeight = window.innerHeight * .5;
 
 function draw(ctx, location){
   console.log("attempting to draw")
@@ -28,14 +29,26 @@ function draw(ctx, location){
 
 
 function App() {
+  // useRef creates an JS object that can be manipulated and is not recreated each render
   const canvasRef = useRef(null);
 
-  const handleCanvasClick=(event)=>{
-    // alert(`You have clicked on mouse coordinates: X: ${event.clientX} Y: ${event.clientY}`);
+  const [coordinates, setCoordinates] = useState([]);
+
+  useEffect(()=>{
     const canvasObj = canvasRef.current;
     const ctx = canvasObj.getContext('2d');
-    draw(ctx, {x: event.clientX, y: event.clientY});
+    ctx.clearRect( 0,0, canvasHeight, canvasWidth );
 
+    // draw all coordinates held in state
+    coordinates.forEach((coordinate)=>{draw(ctx, coordinate)});
+  })
+
+  const handleCanvasClick=(event)=>{
+    // on each click get current mouse location 
+    const currentCoord = { x: event.clientX, y: event.clientY };
+    // add the newest mouse location to an array in state 
+    setCoordinates([...coordinates, currentCoord])
+    console.log("Coordinates", coordinates);
   };
 
 
@@ -45,8 +58,8 @@ function App() {
       <canvas 
         className="App-canvas"
         ref={canvasRef}
-        width={window.innerWidth * .5}
-        height={window.innerHeight * .5}
+        width={canvasWidth}
+        height={canvasHeight}
         onClick={handleCanvasClick} />
 
     </main>
